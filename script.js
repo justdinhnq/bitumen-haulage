efa8f11c4671066086a2cc9e0a7728f37dd9d4df
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2025-04-09 13:53:42 +1200

    use Render as a proxy

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ JFCustomWidget.subscribe("ready", function() {
         try {
             const response = await fetch(
                 // `https://api.jotform.com/form/${formId}/submissions?apiKey=${apiKey}`
-                `https://downer.jotform.com/API/form/${formId}/submissions?apiKey=${apiKey}`
+                //`https://downer.jotform.com/API/form/${formId}/submissions?apiKey=${apiKey}`
+                'https://jotform-proxy.onrender.com/proxy/submissions'
             );
             console.log('response: ', response);
-
             const data = await response.json();
             
             submissionsData = data.content || [];
