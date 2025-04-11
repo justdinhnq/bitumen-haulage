510c9a095b848cfabac1f64062ec07ee4af7abe9
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2025-04-11 12:49:34 +1200

    change source

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ JFCustomWidget.subscribe("ready", function() {
         try {
             const response = await fetch(
                 // `https://api.jotform.com/form/${formId}/submissions?apiKey=${apiKey}`
-                //`https://downer.jotform.com/API/form/${formId}/submissions?apiKey=${apiKey}`
-                'https://jotform-proxy.onrender.com/proxy/submissions'
+                `https://downer.jotform.com/API/form/${formId}/submissions?apiKey=${apiKey}`
+                //'https://jotform-proxy.onrender.com/proxy/submissions'
             );
             console.log('response: ', response);
             const data = await response.json();
