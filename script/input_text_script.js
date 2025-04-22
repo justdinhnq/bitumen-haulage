69b5f26e50cb9d28c147f0eba9ad8807529e215c
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2025-04-23 11:48:41 +1200

    cleaning

--- script/input_text_script.js
+++ script/input_text_script.js
@@ -2,49 +2,6 @@ JFCustomWidget.subscribe("ready", function() {
     const textField = document.getElementById('donwer_text');
     let pollingInterval = null;
 
-    console.log("script for input text is ready!!!!");
-    
-    const jsonKey = JFCustomWidget.getWidgetSettings("key").Key;
-
-    function pollData() {
-        console.log("Poll data with the key name: ", jsonKey);
-        console.log("Data in textfield: ", textField.value);
-        console.log("Data from another field: ", JFCustomWidget.getFormFieldValue("input_9"));
-        console.log("Data from another field...: ", JFCustomWidget.getFormFieldValue("submission-dropdown"));
-
-        if (textField.value && JFCustomWidgetUtils.isJsonString(textField.value)) {
-            const value = JSON.parse(textField.value);
-            console.log("Extracted value for", jsonKey, ":", value);
-
-            // Update text field
-            textField.value = value;
-
-            // Send value to form field
-            JFCustomWidget.sendData({
-                value: value
-            });
-        }
-    }
-
-    // Start polling every 500ms
-    pollData();
-    pollingInterval = setInterval(pollData, 500);
-
-    // Stop polling after 30 seconds to prevent infinite loops
-    setTimeout(() => {
-        if (pollingInterval) {
-            clearInterval(pollingInterval);
-            console.log("Stopped polling for JSON data");
-        }
-    }, 30000);
-});
-
-
-
-JFCustomWidget.subscribe("ready", function() {
-    const textField = document.getElementById('donwer_text');
-    let pollingInterval = null;
-
     JFCustomWidget.getWidgetSettings(function(settings) {
         const jsonKey = JFCustomWidget.getWidgetSettings("key").Key;
         const sourceFieldId = settings.sourceFieldId || 'submission-dropdown';
