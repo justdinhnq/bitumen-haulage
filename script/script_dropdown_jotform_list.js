1299c86c6e57f958a3edb64973055b4b3c1b8b3d
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2025-06-19 08:14:01 +1200

    add log

--- script/script_dropdown_jotform_list.js
+++ script/script_dropdown_jotform_list.js
@@ -26,7 +26,7 @@ JFCustomWidget.subscribe("ready", function() {
             // Populate dropdown
             dropdown.innerHTML = '<option value="">Select Training Session</option>';
             submissionsData.forEach(submission => {
-                console.log('data: ', submission.answers)
+                console.log('data of each submission: ', submission.answers)
 
                 const now = new Date();
                 const endDate = Object.values(submission.answers).find(
