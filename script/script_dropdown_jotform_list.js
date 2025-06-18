42fe89de59eac6778d89ccb7f2891d7ce5c454a9
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2025-06-19 09:34:42 +1200

    add log

--- script/script_dropdown_jotform_list.js
+++ script/script_dropdown_jotform_list.js
@@ -32,7 +32,9 @@ JFCustomWidget.subscribe("ready", function() {
                 const endDate = Object.values(submission.answers).find(
                     answer => answer.name === 'endDatetime'
                 );
-                
+                console.log('End date: ', endDate)
+
+
                 const oneWeekInMs = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
                 if (endDate - now > oneWeekInMs) {
