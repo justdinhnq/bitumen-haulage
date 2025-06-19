fd609888ec63618dcb100ccede7c2f10f114e6d8
Dinh Nguyen <justdinhnq@gmail.com>
2025-06-19 13:15:06 +1200

    check

--- script/script_dropdown_jotform_list.js
+++ script/script_dropdown_jotform_list.js
@@ -34,6 +34,8 @@ JFCustomWidget.subscribe("ready", function() {
                 const end = Object.values(submission.answers).find(
                     answer => answer.name === 'endDatetime'
                 );
+                console.log('End date 1: ', endDate.datetime);
+                console.log('End date 2: ', endDate.ampm);
                 const endDateStr = endDate.datetime + ' ' + endDate.ampm;
                 console.log('End date string: ', endDateStr);
                 const endDate = new Date(endDateStr);
