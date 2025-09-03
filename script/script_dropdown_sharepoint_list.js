92f75dd217db6b574598f925eaa1d4070b3d651d
Dinh Nguyen <justdinhnq@gmail.com>
2025-09-04 11:27:15 +1200

    rename

--- script/script_dropdown_sharepoint_list.js
+++ script/script_dropdown_sharepoint_list.js
@@ -9,7 +9,7 @@ window.JFCustomWidget.subscribe("ready", function (data) {
   var conditional_value = JFCustomWidget.getWidgetSetting("conditional_value");
   var column_to_show = JFCustomWidget.getWidgetSetting("column_to_show");
   var column_to_value = JFCustomWidget.getWidgetSetting("column_to_value");
-  var  tableSP = JFCustomWidget.getWidgetSetting("tableInSharePoint");
+  var  tableSP = JFCustomWidget.getWidgetSetting("table");
   
 
   const dropdown = document.getElementById("lookup_list");
