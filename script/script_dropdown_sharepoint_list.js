c980e67c9c71cb0dfac2c98693a0e8c63f6135b5
Dinh Nguyen <justdinhnq@gmail.com>
2025-09-04 09:43:07 +1200

    wait 3 seconds

--- script/script_dropdown_sharepoint_list.js
+++ script/script_dropdown_sharepoint_list.js
@@ -1,62 +1,57 @@
 const POWER_AUTOMATE_URL =
   "https://prod-14.australiasoutheast.logic.azure.com:443/workflows/95ae937e45fc428ebc665758f7cdd45e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ozvB4cLU1-l2H3o85H7v8nBe1Bm4ZYk4VckfU-6E-xo";
 
-const timestamp = new Date()
-  .toISOString()
-  .replace(/[-:]/g, "")
-  .split(".")[0]
-  .replace(/(\d{2})(\d{2})(\d{2})$/, "_$1_$2_$3");
+// Wait 3 seconds before subscribing to widget ready event
+setTimeout(() => {
+  window.JFCustomWidget.subscribe("ready", function (data) {
+    const site_address = JFCustomWidget.getWidgetSetting("site_address");
+    const conditional_column = JFCustomWidget.getWidgetSetting("conditional_column");
+    const conditional_value = JFCustomWidget.getWidgetSetting("conditional_value");
+    const column_to_show = JFCustomWidget.getWidgetSetting("column_to_show");
+    const column_to_value = JFCustomWidget.getWidgetSetting("column_to_value");
+    const table = JFCustomWidget.getWidgetSetting("table");
 
-// Initialize the widget
-window.JFCustomWidget.subscribe("ready", function (data) {
-  const site_address = JFCustomWidget.getWidgetSetting("site_address");
-  const conditional_column = JFCustomWidget.getWidgetSetting("conditional_column");
-  const conditional_value = JFCustomWidget.getWidgetSetting("conditional_value");
-  const column_to_show = JFCustomWidget.getWidgetSetting("column_to_show");
-  const column_to_value = JFCustomWidget.getWidgetSetting("column_to_value");
-  const table = JFCustomWidget.getWidgetSetting("table");
+    const dropdown = document.getElementById("lookup_list");
 
-  const dropdown = document.getElementById("lookup_list");
+    console.log("Site Address: ", site_address);
+    console.log("Conditional Column: ", conditional_column);  
+    console.log("Conditional Value: ", conditional_value);
+    console.log("Column to Show: ", column_to_show);
+    console.log("Column to Value: ", column_to_value);
+    console.log("Table: ", table);
 
-  console.log("Site Address: ", site_address);
-  console.log("Conditional Column: ", conditional_column);  
-  console.log("Conditional Value: ", conditional_value);
-  console.log("Column to Show: ", column_to_show);
-  console.log("Column to Value: ", column_to_value);
-  console.log("Table: ", table);
-  
-
-  // Fetch contract names from Power Automate
-  fetch(POWER_AUTOMATE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ 
-        site_addr: site_address,
-        conditional_col: conditional_column,
-        conditional_val: conditional_value,
-        col_to_show: column_to_show,
-        col_to_value: column_to_value,
-        table_name: table
-    }), 
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        const option = document.createElement("option");
-        option.value = item.project; // Use project code as the value
-        option.textContent = item.contract; // Display contract name
-        dropdown.appendChild(option);
-      });
-
-      const numberOfOptions = dropdown.options.length;
-      console.log("Number of options: ", numberOfOptions);
+    // Fetch contract names from Power Automate
+    fetch(POWER_AUTOMATE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+          site_addr: site_address,
+          conditional_col: conditional_column,
+          conditional_val: conditional_value,
+          col_to_show: column_to_show,
+          col_to_value: column_to_value,
+          table_name: table
+      }), 
     })
-    .catch((err) => {
-      console.error("Error fetching:", error);
-    });
-});
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((item) => {
+          const option = document.createElement("option");
+          option.value = item.project; // Use project code as the value
+          option.textContent = item.contract; // Display contract name
+          dropdown.appendChild(option);
+        });
+
+        const numberOfOptions = dropdown.options.length;
+        console.log("Number of options: ", numberOfOptions);
+      })
+      .catch((err) => {
+        console.error("Error fetching:", err);
+      });
+  });
+}, 3000);
 
 // Subscribe to form submit event
 window.JFCustomWidget.subscribe("submit", function () {
