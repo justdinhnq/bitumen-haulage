0a512b2d0de73928288149da7b678f975edc830b
Dinh Nguyen <justdinhnq@gmail.com>
2025-09-24 13:10:51 +1200

    get keys

--- script/lookup-to-Jotform-submission.js
+++ script/lookup-to-Jotform-submission.js
@@ -1,26 +1,27 @@
-apiKey = '38253712fa8d8d79431cd7ec2ca697ee';//JFCustomWidget.getWidgetSetting("apiKey")
-// label = JFCustomWidget.getWidgetSetting("submissionLabel")
+JFCustomWidget.subscribe("ready", function() {
+    apiKey = JFCustomWidget.getWidgetSetting("apiKey")
 
-// Fetch submissions
-const fetchSubmissions = async () => {
-    try {
-        submissionID_A = '6291102641966994926'; //document.getElementById("submission-textfield").value;
+    // Fetch submissions
+    const fetchSubmissions = async () => {
+        try {
+            submissionID_A = document.getElementById("submission-textfield").value;
 
-        const url = `https://downer.jotform.com/API/submission/${submissionID_A}?apiKey=${apiKey}`;
-        console.log('Fetch URL: ', url);
-        const response = await fetch(
-            url
-        );
-        const data = await response.json();
-        
-        console.log('submission info: ', data.content);
-        
-        handleSelection(data.content);
-    } catch (error) {
-        console.error('Error fetching submissions:', error);
-        dropdown.innerHTML = '<option value="">Error loading submissions</option>';
-    }
-};
+            const url = `https://downer.jotform.com/API/submission/${submissionID_A}?apiKey=${apiKey}`;
+            console.log('Fetch URL: ', url);
+            const response = await fetch(
+                url
+            );
+            const data = await response.json();
+            
+            console.log('submission info: ', data.content.answers);
+            
+            handleSelection(data.content.answers);
+        } catch (error) {
+            console.error('Error fetching submissions:', error);
+            dropdown.innerHTML = '<option value="">Error loading submissions</option>';
+        }
+    };
+});
 
 // Listen for changes in the textfield and run fetchSubmissions when it has a value
 const textfield = document.getElementById("submission-textfield");
@@ -31,19 +32,20 @@ textfield.addEventListener("input", function() {
 });
 
 // send message to the channel
-function handleSelection(data) {
-  for (const element of data) {
+function handleSelection(input) {
+  for (const element of input) {
     console.log('Selected user details:', element);
-    for (const key in element) {
-      let sender = 'talk_channel_'+key
+    for (const field in element) {
+        let key = field.name;
+        let sender = 'talk_channel_'+key
   
-      console.log("[Sender]Sender name: ", sender)
-      const channel = new BroadcastChannel(sender);
-      
-      const value = element[key]; // Get the selected value
-  
-      channel.postMessage(value); 
-      console.log('Message sent:', value);
+        console.log("[Sender]Sender name: ", sender)
+        const channel = new BroadcastChannel(sender);
+        
+        const value = field.answer !== undefined ? field.answer : '';
+    
+        channel.postMessage(value); 
+        console.log('Message sent:', value);
     }
     
   }
